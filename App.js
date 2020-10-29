d9a030b16daf9c92749e5b1cef61bb3bf5a4df74
Umesh Ahir <70050760+Umeshahir2000@users.noreply.github.com>
2020-10-29 12:52:56 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './index.css';
+import ToDOList from './toDoList';
+
+const App = () =>{
+
+    const [inputlist, setinputlist] = useState("");
+    const[items, setitems] = useState([]);
+
+    const itemEvent = (event) => {
+        setinputlist(event.target.value);
+    };
+
+    const listItem = () => {
+        setitems((oldItmes) => {
+            return [...oldItmes, inputlist];
+        });
+        setinputlist("");
+    };
+
+    const deleteItem = (id) => {
+        console.log("deleted");
+
+        setitems( (oldItmes) => {
+            return oldItmes.filter( (arrEle, index) =>{
+                return index !== id ;
+            });
+        } );
+    };
+
+    return(
+        <>
+            <div className= "main_div">
+                <div className= "center_div">
+                <h1> ToDO List</h1>
+                <br/>
+                <input type="text" placeholder="Add a Items" onChange= {itemEvent} value= {inputlist}/> 
+                <button onClick = {listItem}> + </button>
+                
+                <ol>
+                {    items.map( (itemval,index)  => {
+                     return (
+                         <>
+                         <div>
+                        
+                            <ToDOList
+                            key= {index}
+                            id= {index}
+                            text= {itemval}
+                            onSelect= {deleteItem}
+                            />
+                            </div>
+                        </>
+                        );
+                    })
+                }
+                </ol>
+                </div>
+            </div>
+        </>
+)
+};
+export default App;
